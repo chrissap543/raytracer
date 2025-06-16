b5eff008a29cf7e2275a144a209aca630e848cfc
chrissap543 <sapinskichristopher@gmail.com>
2025-06-16 12:17:24 -0700

    moved to cmake

--- scripts/profiling_input_parameters.sh
+++
@@ -1,7 +0,0 @@
-input_file=../profiling_data/CPU/csv/profiling_data.csv
-output_dir=../profiling_data/CPU/csv
-widths=("600" "800" "1000" "1200")
-samples=("10" "30" "50")
-depths=("10" "20" "30" "40")
-number_grid=("0" "5" "10")
-number_grid_list=("0 5 10")
